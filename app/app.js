7a28400b77bf4efcc6e59d66a0dd0df981ddab1c
hebbeh <annpaa91@gmail.com>
2016-12-02 14:16:05 +0100

    simple css added, + initiated first app

--- app/app.js
+++ app/app.js
@@ -1,14 +1,47 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('loneDog', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
 ]).
+
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+
+// Define the `phonecatApp` module
+var loneDog = angular.module('loneDog', []);
+
+// main-controller
+loneDog.controller('MainCtrl', function MainCtrl($scope) {
+  $scope.phones = [
+    {
+      name: 'Bladder',
+      //snippet: 'Fast just got faster with Nexus S.'
+    }, {
+      name: 'Hygiene',
+      //snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'Hunger',
+      //snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'Fun',
+      //snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'Thirst',
+      //snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'Sleep',
+      //snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'Stress',
+      //snippet: 'The Next, Next Generation tablet.'
+    }
+  ];
+});
